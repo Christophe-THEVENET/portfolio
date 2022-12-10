3c3108d1005d908fc2ecbe80eb0f5cf7865e4623
Christophe THEVENET <christophethevenet2.0@gmail.com>
2022-12-10 10:26:24 +0100

    ajout guide navigation a la molette

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ import '../css/main.scss';
 // ajout supp classe open sur les boutons hamburger
 let toggle = document.querySelector('.header__nav__btn');
 
-
 toggle.addEventListener('click', () => {
   // permet de savoir si le menu est ouvert
   document.body.classList.toggle('open');
@@ -20,3 +19,13 @@ links.forEach((link) => {
   });
 });
 
+// apparition icone scroll décalé
+let scrollMouseIcon = document.querySelector('.scroll-icon');
+let textUseScroll = document.querySelector('.text-use-mouse');
+let textNbPages = document.querySelectorAll('.text-nb-pages');
+
+setTimeout(() => {
+  scrollMouseIcon.style.opacity = 1;
+  textUseScroll.style.opacity = 1;
+
+}, 7000);
